b1a98d5d89b97151754d17269b5c0fbc89757c5a
odida7 <danielodida@yahoo.com>
2024-03-15 23:03:41 +0300

    first-commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,36 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ToasterProvider } from "@/lib/ToasterProvider";
+import LeftSideBar from "@/components/layout/LeftSideBar";
+import TopBar from "@/components/layout/TopBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Oda_Admin",
+  description: "Ecommerce admin page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToasterProvider />
+          <div className="flex max-lg:flex-col text-grey-1">
+            <LeftSideBar />
+            <TopBar />
+            <div className="flex-1">{children}</div>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
